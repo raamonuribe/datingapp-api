6146dbb0b3bc7f381aeb228f94180bc33843c87e
Ramon Uribe <ramon@ramonuribe.com>
2021-04-06 11:32:19 -0700

    Implemented repository pattern, and started using automapper

--- Extensions/ApplicationServiceExtensions.cs
+++ Extensions/ApplicationServiceExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using API.Data;
+using API.Data.Repositories;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
@@ -12,6 +14,8 @@ namespace API.Extensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(config.GetConnectionString("DefaultConnection"));
